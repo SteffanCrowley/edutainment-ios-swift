794c492ba0c3409be67554c2f428520701a2cb55
Steffan Crowley <steffancrow@gmail.com>
2022-08-22 15:47:00 -0700

    Initial Commit

---
+++ edutainment-ios-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  edutainment-ios-swift
+//
+//  Created by steffan crowley on 8/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ edutainment-ios-swift/edutainment_ios_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainment_ios_swiftApp.swift
+//  edutainment-ios-swift
+//
+//  Created by steffan crowley on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct edutainment_ios_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
