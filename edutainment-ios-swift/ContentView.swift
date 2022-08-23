fb8ff805c543eef06d05cc43b287a287382a0e08
Steffan Crowley <steffancrow@gmail.com>
2022-08-23 10:42:17 -0700

    get basic functionality working

--- edutainment-ios-swift/ContentView.swift
+++ edutainment-ios-swift/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstMultiplier = 0
+    @State private var secondMultiplier = Int.random(in: 0...10)
+    @State private var answer = 0
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Stepper("\(firstMultiplier.formatted())", value: $firstMultiplier, in:0...10, step: 1)
+                }header: {Text("Set the first multiplier")}
+                
+                Section {
+                    Text("\(secondMultiplier)")
+                }header: {Text("Second multiplier")}
+
+                Section {
+                    TextField("Enter your score", value: $answer, format: .number)
+                        .textFieldStyle(.roundedBorder)
+                }header: {Text("Input your solution")}
+
+            }
+
+            .navigationTitle("Math Quiz")
+            .toolbar {
+                Button("Calculate", action: calculate)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
+        }
+        
+    }
+    
+    func calculate() {
+        let solution = firstMultiplier * secondMultiplier
+        
+        if (solution == answer) {
+            alertTitle = "CORRECT!"
+            alertMessage = "You are correct!"
+        } else {
+            alertTitle = "Wrong"
+            alertMessage = "Sorry, you are wrong"
+        }
+        showingAlert = true
+        secondMultiplier = Int.random(in: 0...10)
     }
 }
 
